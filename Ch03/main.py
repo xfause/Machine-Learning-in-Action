ebc6a34e0883d43a08081050c4e67f46b64c69a1
xfause <919017553@qq.com>
2017-07-18 22:53:49 +0800

    commit-5

--- Ch03/main.py
+++ Ch03/main.py
@@ -7,9 +7,15 @@ myDat,labels = trees.CreateDataSet()
 
 # 特征值的增加会使香农熵增加
 # myDat[0][-1] = 'maybe'
-#print(myDat)
+# print(myDat)
 # print(trees.calcShannonEnt(myDat))
 
 # 三个参数 数据集 要划分的特征 特征值
 # 在数据集中找特征等于特征值的项
-print(trees.splitDataSet(myDat,0,1))
+# print(trees.splitDataSet(myDat,0,1))
+
+# 选择最适合分类的一个特征
+# print(trees.chooseBestFeatureToSplit(myDat))
+
+# 树结构 字典
+print(trees.createTree(myDat,labels))
--- Ch03/trees.py
+++ Ch03/trees.py
@@ -41,4 +41,42 @@ def chooseBestFeatureToSplit(dataSet):
     bestInfoGain = 0.0
     bestFeature = -1
     for i in range(numFeatures):
-        
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet,i,value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob*calcShannonEnt(subDataSet)
+        InfoGain = baseEntropy-newEntropy
+        if (InfoGain > bestInfoGain):
+            bestInfoGain = InfoGain
+            bestFeature = i
+    return bestFeature
+
+# 计算有多少种分类
+def majorityCnt(classList):
+    classCnt = {}
+    for vote in classList:
+        if vote not in classCnt.keys(): classCnt[vote] = 0
+        classCnt[vote] += 1
+    sortedClassCnt = sorted(classCnt.iteritems(),
+    key=operator.itemgetter(1), reverse=True)
+    return sortedClassCnt[0][0]
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeature = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeature]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeature])
+    featValues = [example[bestFeature] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeature,value),subLabels)
+    return myTree
